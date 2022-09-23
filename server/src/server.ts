68fe1cbb43fa0031ae58d2024b6867e991dd6af1
Gabriel Brito de Almeida <gabriel.brito@angellira.com.br>
2022-09-23 14:35:13 -0300

    feat: update server

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,29 @@ import express from "express";
 const app = express();
 
 app.get("/", function (req, res) {
-  return res.status(201).json([{ message: "Hello World" }]);
+  return res.status(201).json([
+    {
+      id: 1,
+      title: "Aluguel",
+      price: 1000,
+      category: "Casa",
+      date: "2021-02-01T00:00:00",
+    },
+    {
+      id: 2,
+      title: "Carro",
+      price: 1000,
+      category: "Transporte",
+      date: "2021-02-01T00:00:00",
+    },
+    {
+      id: 3,
+      title: "Viagem",
+      price: 1000,
+      category: "laser",
+      date: "2021-02-01T23:59:59",
+    },
+  ]);
 });
 
 app.listen(3333, function () {
